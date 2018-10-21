e7582dd931dc97a76b283b9cd7ec7b5b743615e8
Konstantin <2447785@gmail.com>
2018-10-21 12:38:24 +0300

    Vue-yandex-metrika integration

---
+++ test/module.test.js
@@ -0,0 +1,26 @@
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000
+process.env.PORT = process.env.PORT || 5060
+process.env.NODE_ENV = 'production'
+
+const { Nuxt, Builder } = require('nuxt')
+const request = require('request-promise-native')
+
+const url = path => `http://localhost:${process.env.PORT}${path}`
+const get = path => request(url(path))
+
+describe('VueYandexMetrika', () => {
+  let nuxt
+
+  afterEach(async () => {
+    await nuxt.close()
+  })
+
+  test('default', async () => {
+    nuxt = new Nuxt(require('./fixture/nuxt.config'))
+    await new Builder(nuxt).build()
+    await nuxt.listen(process.env.PORT)
+    let html = await get('/')
+
+    expect(html).toContain('Works!')
+  })
+})
